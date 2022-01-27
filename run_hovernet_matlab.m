04586c4b1b61a5078ebc1dbdd06588254e760173
kyu <kyuhan14@gmail.com>
2022-01-27 07:58:29 -0800

    first commit

---
+++ run_hovernet_matlab.m
@@ -0,0 +1,20 @@
+% add python executor 
+pyExec = 'C:\Users\Pei-Hsun\anaconda3\envs\hovernet\python.exe';
+pyRoot = fileparts(pyExec);
+p = getenv('PATH');
+p = strsplit(p, ';');
+addToPath = {
+    pyRoot
+    fullfile(pyRoot, 'Library', 'mingw-w64', 'bin')
+    fullfile(pyRoot, 'Library', 'usr', 'bin')
+    fullfile(pyRoot, 'Library', 'bin')
+    fullfile(pyRoot, 'Scripts')
+    fullfile(pyRoot, 'bin')
+    };
+p = [addToPath(:); p(:)];
+p = unique(p, 'stable');
+p = strjoin(p, ';');
+setenv('PATH', p);
+
+% run command line 
+system('python wsi2tile.py')
