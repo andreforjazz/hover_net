ba87eec3944dd9f1846160fab4b47063d136ba8a
labuser1 <kyuhan14@gmail.com>
2023-03-14 17:09:07 -0400

    kyu_comment_out_env.yml

--- infer/wsi.py
+++ infer/wsi.py
@@ -756,8 +756,9 @@ class InferManager(base.InferManager):
         #TO-DO: take list of wsi as input. now it's handled inside of the package
         wsi_path_list1 = pd.read_excel(r"\\shelter\Kyu\skin_aging\clue_cohort\CLUE_image_list_230207_v2.xlsx", engine='openpyxl')
         wsi_path_list2 = wsi_path_list1[(wsi_path_list1['student score'] > 1)]
-        wsi_path_list3 = wsi_path_list2.set_index('index')
-        wsi_path_list4 = wsi_path_list3.sort_index()
+        wsi_path_list4 = natsorted(wsi_path_list2)
+        # wsi_path_list3 = wsi_path_list2.set_index('index')
+        # wsi_path_list4 = wsi_path_list3.sort_index()
         wsi_path_list = [os.path.join(self.input_dir, _) for _ in wsi_path_list4['filename'].tolist()]
 
         for wsi_path in wsi_path_list[:]:
